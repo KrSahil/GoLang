2315618083bc9c23970f40cba85df269530631ae
Sahil Kumar Shah <Sahil.shah_1@hotmail.com>
2023-01-22 21:30:02 +0530

    Hello World

---
+++ hello.go
@@ -0,0 +1,9 @@
+package main
+
+//fmt is the package where printing is there
+import "fmt"
+
+//main function will be automatically called when you start the application
+func main(){
+	fmt.Println("Hello World")
+}
